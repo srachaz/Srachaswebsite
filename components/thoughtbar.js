8978dd09f2f7791d69beafe851895ef6705643dc
Sarah Zhang <sarahrjzhang@gmail.com>
2023-08-21 23:11:15 -0700

    updating thoughts page, minor phone UI fixes

---
+++ components/thoughtbar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const Thoughtbar = () => {
+  return (
+    <nav className="">
+        <div className="container mx-auto flex flex-col text-gray-400 items-center">
+            <div>Interesting Lives</div>
+            <div>Interesting Lives</div>
+            <div>Interesting Lives</div>
+
+
+            
+        </div>
+        
+      </nav>  
+  );
+};
+
+export default Thoughtbar;
--- pages/thoughts.js
+++ pages/thoughts.js
@@ -1,16 +1,46 @@
 import Navbar from "@/components/navbar";
+import Thoughtbar from "@/components/thoughtbar";
 
 export default function Home() {
     return(
         <>
         <Navbar />
-        <div className="flex items-center justify-between flex-col">
-            <div className="flex flex-col justify-center mt-4">
+        {/* <div class="flex">
+            <div class="mr-10">
+            <Thoughtbar />
+                </div></div> */}
         
+        <div className="flex items-center justify-between flex-col">
+            <div className="flex flex-col justify-center mt-4 mx-6 sm:mx-40 md:mx-60 lg:mx-80">
+        
+            <h1 className="text-3xl text-[#737df0] font-bold text-center">Interesting Lives</h1> 
+                <div className="text-sm text-[#737df0] font-semibold text-center">21 Aug 23</div> 
+                
+               <div className=" pt-5 pb-5">
+               Some people&apos;s lives can be written into multiple books, span multiple Netflix seasons. Reading their wikipedia pages is thrilling—Chelsea Manning, Harriet Tubman, Ted Kaczynski, to name a few. Their lives do violate legalities and involve physical, harrowing struggles. But reading about it is an adventure in itself.<br/> <br/>
+
+People enjoy hearing interesting stories about people&apos;s lives. That&apos;s why media is so popular (Anime, TikTok, Hollywood films)—people can vicariously live an exciting adventure. But why live vicariously when you can live it out for yourself?<br/> <br/>
+
+Having an interesting life is easier than people think, if you make the conscious effort to do things that are interesting.<br/> <br/>
+
+Example: I was deeply unhappy from November to January (college apps & a time-consuming class I despised). But when the storm passed, I wanted to take control of my life and live exciting adventures. I targeted common hobbies I had: going to the gym was getting mundane, so I decided to buy a half marathon ticket that was to take place in May, and the morning after our senior ball.<br/> <br/>
+
+I was not a runner—I didn&apos;t sports in high school and I lifted weights at the gym. But when I bought the ticket and started training, I suddenly had a mountain to climb and a summit to reach. Some of my friends thought I was delusional, but that made the goal more enticing.<br/> <br/>
+
+I missed the senior ball afterparty to sleep early, but I only got 3 hours of sleep before I scrambled to BART, sprinting through the doors as it closed. I was shitting my pants the entire train ride, because I hadn&apos;t been training the past 2 weeks due to an injury. My goal was quite literally to not get shuttled off the course and actually complete it.<br/> <br/>
+
+But I ran the half marathon, across the West Oakland cranes, across the Bay Bridge and speeding cars. And it was one of the most surreal experiences ever. It is a core memory. It is part of my identity capital. It is a story I can tell forever, to anyone.<br/> <br/>
+
+To me this is an interesting story, but something interesting for one person might not seem exciting from another&apos;s perspective. Personal growth is all that matters.<br/> <br/>
+
+Of course I&apos;m not saying do things for the purpose of appeasing other people — that&apos;s something I dislike about social media, I don&apos;t want to revolve my life for the purpose of catering towards other people&apos;s interests. But doing crazy things that YOU would be captivated to hear from another person—living like that, in my opinion, makes life interesting. And I aspire to live an interesting, crazy life.<br/> <br/> <br/> <br/> 
+            </div>
+                
+                
                 <h1 className="text-3xl text-[#737df0] font-bold text-center">Time is Finite</h1> 
                 <div className="text-sm text-[#737df0] font-semibold text-center">17 Jun 23</div> 
                 
-               <div className=" pt-5 mx-6 sm:mx-40 pb-5">
+               <div className=" pt-5 pb-20">
                Everyone says that life is a long long journey, that life is a marathon. <br/> <br/>
 
 I was reading The 3 Body Problem, and I fell upon this passage:
