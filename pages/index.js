5861006a6bd596e366858c3f35aca25c87916eb4
Sarah Zhang <sarahrjzhang@gmail.com>
2024-03-19 19:44:27 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
             
               <h1 className="text-3xl text-[#737df0] font-bold">Hi, I&apos;m Sarah!</h1>
               
-              <div className="text-sm"><br/>UCLA first-year excited about crypto and soon-to-be world class DJ. </div>
+              <div className="text-sm"><br/>UCLA first-year excited about crypto and soon-to-be world-class DJ. </div>
               <div className="text-sm pt-3">Life changed after I sneaked into <a href="https://twitter.com/sracha_z/status/1627588736870682625" class="text-[#737df0] font-bold">Treehacks</a>
               , where I realized that life is best lived <a href="https://medium.com/@sracha/interesting-life-85ccf33054e2" class="text-[#737df0] font-bold">interestingly</a>. I love crypto, which I'm exploring further on campus through <a href="https://www.blockchainatucla.com/" class="text-[#737df0] font-bold"> Blockchain @ UCLA</a> and the <a href="https://uclaconnectionlab.org/" class="text-[#737df0] font-bold"> Connection Lab</a>, and I also 
               lead growth at <a href="https://twitter.com/ebisu_finance" class="text-[#737df0] font-bold">Ebisu Finance</a>, a DeFi startup building a stablecoin backed by liquid restaking tokens. My first crypto conference at  <a href="https://medium.com/@sracha/ethdenver-2024-fc0e2153d1a3" class="text-[#737df0] font-bold"> ETHDenver</a> was an unforgetabble experience—I hope to travel the world and make more amazing friends! </div>
