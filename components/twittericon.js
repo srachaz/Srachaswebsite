ffd2fd09e0f96a0a9e57f61d66cfcf57114b65fa
Sarah Zhang <sarahrjzhang@gmail.com>
2024-07-18 16:34:41 +0900

    update

--- components/twittericon.js
+++ components/twittericon.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const TwitterIcon = () => {
   return (
     <a href="https://twitter.com/sracha_z" target="_blank" rel="noopener noreferrer">
-      <div className="w-4 h-4">
+      <div className="text-3xl text-[#737df0] font-bold w-4 h-4">
         <Image src="/icons/twitter.svg" alt="Twitter Icon" width={24} height={24} />
       </div>
     </a>
--- pages/index.js
+++ pages/index.js
@@ -23,28 +23,22 @@ export default function Home() {
     <title>Sarah Zhang</title>
    </Head>
   <Navbar />
-      <div className="flex items-center justify-between flex-col p-5">
-        <div className="flex flex-row gap-6 justify-center mt-12">
-          <div className = "align-right leading-4">
-            <div><Image src="/headshot.png" alt="headshot"  width={200} height={200}/> </div>
-              <div className="text-right text-sm pt-2">VAN🇨🇦 → SD → SF & LA🌁 </div>
-              <div><br/></div>
-              <div className="flex flexrow justify-end space-x-4"><GithubIcon/><TwitterIcon/><LinkedInIcon/><MailIcon/></div>
+      <div className="flex items-center flex-col p-5 min-h-screen">
+        <div className="flex flex-row justify-center pt-20">
+          
+          <div>
+           
+              <h1 className="text-3xl text-[#737df0] font-bold ">Sarah Z </h1> 
+             
               
-          </div>
-          <div className="w-1/2 ">
-            
-              <h1 className="text-3xl text-[#737df0] font-bold">Hi, I&apos;m Sarah!</h1>
+              <div className="text-sm pt-3">Life is best lived <a href="https://medium.com/@sracha/interesting-life-85ccf33054e2" class="text-[#737df0] font-bold">interestingly</a>. 
+              <br/> I like <a href="https://x.com/sracha_z" class="text-[#737df0] font-bold"> crypto.</a> 
+              <br/>Exploring at <a href="https://twitter.com/ebisu_finance" class="text-[#737df0] font-bold">Ebisu Finance.</a>
+              <br/> 
+             
+              </div>
               
-              <div className="text-sm"><br/>UCLA first-year excited about crypto and soon-to-be world-class DJ. </div>
-              <div className="text-sm pt-3">Life changed after I sneaked into <a href="https://twitter.com/sracha_z/status/1627588736870682625" class="text-[#737df0] font-bold">Treehacks</a>
-              , where I realized that life is best lived <a href="https://medium.com/@sracha/interesting-life-85ccf33054e2" class="text-[#737df0] font-bold">interestingly</a>. I love crypto, which I&apos;m exploring further on campus through <a href="https://www.blockchainatucla.com/" class="text-[#737df0] font-bold"> Blockchain @ UCLA</a> and the <a href="https://uclaconnectionlab.org/" class="text-[#737df0] font-bold"> Connection Lab</a>, and I also 
-              lead growth at <a href="https://twitter.com/ebisu_finance" class="text-[#737df0] font-bold">Ebisu Finance</a>, a DeFi startup building a stablecoin backed by liquid restaking tokens. My first crypto conference at  <a href="https://medium.com/@sracha/ethdenver-2024-fc0e2153d1a3" class="text-[#737df0] font-bold"> ETHDenver</a> was an unforgettable experience—I hope to travel more of the world and make more amazing friends! </div>
-              <div className="text-sm pt-3">In my freetime, I&apos;ll either be running around LA, swimming, DJing, going on an adventure, self-fulfilling my prophecies, or drinking matcha at new cafes! When I have too much freetime, I become very existential...</div>
-              <div className="text-sm pt-3">Still reading: Dune & Foundation</div>
-              {/* <div className="text-sm pt-8"></div> */}
-            
-            <div className="text-sm text-gray-300 relative inline-block border-t top-3">Built & Designed by me </div>
+             
           </div>
           
         </div>
